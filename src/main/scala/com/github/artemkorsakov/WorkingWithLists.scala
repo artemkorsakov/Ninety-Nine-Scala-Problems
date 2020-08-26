47e8ba99e15a9781f6ed0fb548d43a42a1e715d9
Artem Korsakov <kor@infra.guru>
2020-08-26 10:01:00 +0300

    P10 done

--- src/main/scala/com/github/artemkorsakov/WorkingWithLists.scala
+++ src/main/scala/com/github/artemkorsakov/WorkingWithLists.scala
@@ -17,4 +17,60 @@ object WorkingWithLists {
     case _             => throw new NoSuchElementException
   }
 
+  @tailrec
+  def nth[T](idx: Int, list: List[T]): T =
+    if (idx < 0 || idx >= list.length) {
+      throw new NoSuchElementException
+    } else if (idx == 0) {
+      list.head
+    } else {
+      nth(idx - 1, list.tail)
+    }
+
+  def length[T](list: List[T]): Int = list match {
+    case Nil       => 0
+    case _ :: tail => length(tail) + 1
+  }
+
+  def reverse[T](list: List[T]): List[T] = list match {
+    case h :: Nil  => List(h)
+    case h :: tail => reverse(tail) :+ h
+  }
+
+  @tailrec
+  def isPalindrome[T](list: List[T]): Boolean = list match {
+    case Nil       => true
+    case _ :: Nil  => true
+    case h :: tail => h.equals(last(tail)) && isPalindrome(tail.dropRight(1))
+  }
+
+  def flatten(ls: List[Any]): List[Any] = ls flatMap {
+    case ms: List[_] => flatten(ms)
+    case e           => List(e)
+  }
+
+  def compress[T](ls: List[T]): List[T] = ls.foldLeft(List.empty[T]) { (res, item) =>
+    if (res.lastOption.contains(item)) {
+      res
+    } else {
+      res :+ item
+    }
+  }
+
+  def pack[T](ls: List[T]): List[List[T]] = ls.foldLeft(List.empty[List[T]]) { (res, item) =>
+    if (res.lastOption.getOrElse(List.empty[T]).contains(item)) {
+      res.dropRight(1) ++ List(res.last ++ List(item))
+    } else {
+      res ++ List(List(item))
+    }
+  }
+
+  def encode[T](ls: List[T]): List[(Int, T)] = ls.foldLeft(List.empty[(Int, T)]) { (res, item) =>
+    if (res.nonEmpty && res.last._2.equals(item)) {
+      res.dropRight(1) :+ (res.last._1 + 1, item)
+    } else {
+      res :+ (1, item)
+    }
+  }
+
 }
--- src/test/scala/com/github/artemkorsakov/WorkingWithListsTests.scala
+++ src/test/scala/com/github/artemkorsakov/WorkingWithListsTests.scala
@@ -24,4 +24,141 @@ class WorkingWithListsTests extends AnyFlatSpec {
     }
   }
 
+  "P03 (*)" should "find the Kth element of a list." in {
+    nth(2, List(1, 1, 2, 3, 5, 8)) shouldBe 2
+    nth(0, List(1, 1, 2, 3, 5, 8)) shouldBe 1
+    nth(5, List(1, 1, 2, 3, 5, 8)) shouldBe 8
+    nth(4, List(1, 1, 2, 3, 5, 8)) shouldBe 5
+    assertThrows[NoSuchElementException] {
+      nth(-1, List(1, 1, 2, 3, 5, 8))
+    }
+    assertThrows[NoSuchElementException] {
+      nth(6, List(1, 1, 2, 3, 5, 8))
+    }
+  }
+
+  "P04 (*)" should "find the number of elements of a list." in {
+    WorkingWithLists.length(List(1, 1, 2, 3, 5, 8)) shouldBe 6
+  }
+
+  "P05 (*)" should "reverse a list." in {
+    reverse(List(1, 1, 2, 3, 5, 8)) shouldBe List(8, 5, 3, 2, 1, 1)
+  }
+
+  "P06 (*)" should "find out whether a list is a palindrome." in {
+    isPalindrome(List(1, 2, 3, 2, 1)) shouldBe true
+  }
+
+  "P07 (*)" should "flatten a nested list structure." in {
+    flatten(List(List(1, 1), 2, List(3, List(5, 8)))) shouldBe List(1, 1, 2, 3, 5, 8)
+  }
+
+  "P08 (*)" should "eliminate consecutive duplicates of list elements." in {
+    compress(
+      List(
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("b"),
+        Symbol("c"),
+        Symbol("c"),
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("d"),
+        Symbol("e"),
+        Symbol("e"),
+        Symbol("e"),
+        Symbol("e")
+      )
+    ) shouldBe List(
+      Symbol("a"),
+      Symbol("b"),
+      Symbol("c"),
+      Symbol("a"),
+      Symbol("d"),
+      Symbol("e")
+    )
+  }
+
+  "P09 (*)" should "pack consecutive duplicates of list elements into sublists." in {
+    pack(
+      List(
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("b"),
+        Symbol("c"),
+        Symbol("c"),
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("d"),
+        Symbol("e"),
+        Symbol("e"),
+        Symbol("e"),
+        Symbol("e")
+      )
+    ) shouldBe List(
+      List(Symbol("a"), Symbol("a"), Symbol("a"), Symbol("a")),
+      List(Symbol("b")),
+      List(Symbol("c"), Symbol("c")),
+      List(Symbol("a"), Symbol("a")),
+      List(Symbol("d")),
+      List(Symbol("e"), Symbol("e"), Symbol("e"), Symbol("e"))
+    )
+  }
+
+  "P10 (*)" should "run-length encoding of a list." in {
+    encode(
+      List(
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("b"),
+        Symbol("c"),
+        Symbol("c"),
+        Symbol("a"),
+        Symbol("a"),
+        Symbol("d"),
+        Symbol("e"),
+        Symbol("e"),
+        Symbol("e"),
+        Symbol("e")
+      )
+    ) shouldBe List(
+      (4, Symbol("a")),
+      (1, Symbol("b")),
+      (2, Symbol("c")),
+      (2, Symbol("a")),
+      (1, Symbol("d")),
+      (4, Symbol("e"))
+    )
+  }
+
+  "P11 (*)" should "" in {}
+
+  "P12 (*)" should "" in {}
+
+  "P13 (*)" should "" in {}
+
+  "P14 (*)" should "" in {}
+
+  "P15 (*)" should "" in {}
+
+  "P16 (*)" should "" in {}
+
+  "P17 (*)" should "" in {}
+
+  "P18 (*)" should "" in {}
+
+  "P19 (*)" should "" in {}
+
+  "P20 (*)" should "" in {}
+
+  "P21 (*)" should "" in {}
+
+  "P22 (*)" should "" in {}
+
 }
